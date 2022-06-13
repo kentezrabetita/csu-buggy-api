896d6cc60409b2c3c04138ba1e8d6dcd090dea2a
kentezrabetita <kentezrabetita@gmail.com>
2022-06-13 14:21:44 +0800

    remove resolvers, types, and model for notes

--- schema/index.js
+++ schema/index.js
@@ -1,8 +1,6 @@
 const { GraphQLObjectType, GraphQLSchema } = require('graphql');
 const { GET_ALL_USERS, GET_USER } = require('./queries/user');
 const { CREATE_USER, DELETE_USER, LOGIN_USER } = require('./mutations/user');
-const { CREATE_NOTE } = require('./mutations/note');
-const { GET_USER_NOTES, GET_ALL_NOTES } = require('./queries/note');
 const {
   ADD_VISITOR,
   EDIT_VISITOR,
@@ -15,8 +13,6 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     getAllUsers: GET_ALL_USERS,
     getUser: GET_USER,
-    getUserNotes: GET_USER_NOTES,
-    getAllNotes: GET_ALL_NOTES,
     getAllVisitors: GET_ALL_VISITORS,
   },
 });
@@ -26,7 +22,6 @@ const Mutation = new GraphQLObjectType({
   fields: {
     createUser: CREATE_USER,
     deleteUser: DELETE_USER,
-    createNote: CREATE_NOTE,
     loginUser: LOGIN_USER,
     addVisitor: ADD_VISITOR,
     editVisitor: EDIT_VISITOR,
